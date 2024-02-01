f259ff8e9edc18073438cdf695ba019990bbad1d
Hossam Rakha <hossamrakha0@gmail.com>
2024-02-01 18:51:05 +0200

    Initial Commit

---
+++ sample-movies-app-ios/sample_movies_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_movies_app_iosApp.swift
+//  sample-movies-app-ios
+//
+//  Created by hossam on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct sample_movies_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sample-movies-app-iosUITests/sample_movies_app_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sample_movies_app_iosUITestsLaunchTests.swift
+//  sample-movies-app-iosUITests
+//
+//  Created by hossam on 01/02/2024.
+//
+
+import XCTest
+
+final class sample_movies_app_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
